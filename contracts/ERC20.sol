7c8f299d7f61126111c2df362e3c5b1bd8146bcb
smrkarkii <076bct080.smriti@pcampus.edu.np>
2024-05-22 12:00:21 +0545

    Final changes in ERC20 contract

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -6,22 +6,20 @@ pragma solidity ^0.8.9;
 contract ERC20 {
     //state variable
     uint private totalTokens; //total no of tokens
-    string public token; //Token name
-    string public sym; //Token symbol
+    string private token; //Token name
+    string private sym; //Token symbol
     address public owner; //contract owner
 
     mapping(address => uint) balances;
     mapping(address => mapping(address => uint)) allowances;
 
     //constructor for the token
-    constructor(uint _totalTokens, string memory _name, string memory _sym) {
-        totalTokens = _totalTokens;
+    constructor(string memory _name, string memory _sym) {
         token = _name;
         sym = _sym;
         owner = msg.sender;
-        balances[msg.sender] = _totalTokens;
 
-        emit tokenTransferred(address(0), msg.sender, _totalTokens);
+        // emit tokenTransferred(address(0), msg.sender, _totalTokens);
     }
 
     //modifier
@@ -53,6 +51,10 @@ contract ERC20 {
         return totalTokens;
     }
 
+    function decimals() public pure returns (uint8) {
+        return 18;
+    }
+
     //function to return the balance of address
     function balanceOf(address _owner) public view returns (uint) {
         return balances[_owner];
@@ -60,6 +62,7 @@ contract ERC20 {
 
     //function to send some token from sender to receiver
     function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_to != address(0), "can't trasfer to the given address 0X00");
         require(
             balances[msg.sender] >= _value,
             "You can't transfer tokens more than you have"
@@ -73,6 +76,7 @@ contract ERC20 {
     //function to allow 3rd party to transfer tokens
 
     function approve(address sender, uint value) public returns (bool) {
+        require(sender != address(0), "can't approve the given address 0X00");
         allowances[msg.sender][sender] = value;
         emit tokenApproved(msg.sender, sender, value);
         return true;
@@ -85,6 +89,8 @@ contract ERC20 {
         uint256 _value
     ) public returns (bool) {
         require(balances[_from] >= _value, "Not enough tokens");
+        require(_to != address(0), "can't trasfer to the given address 0X00");
+        require(_from != address(0), "can't trasfer from given address 0X00");
         require(
             allowances[_from][msg.sender] >= _value,
             "You can only spend the approved no of tokens"
@@ -98,6 +104,8 @@ contract ERC20 {
 
     //function to mint token to some address
     function mint(address to, uint256 value) public onlyOwner {
+        require(to != address(0), "Can't be transferredto given account 0X00");
+
         balances[to] = balances[to] + value;
         totalTokens = totalTokens + value;
         emit tokenMinted(to, value);
